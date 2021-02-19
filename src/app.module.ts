9ce19f787f600e1ce90925e0a796509dddd1fe85
Eagle19243 <eagle19243@gmail.com>
2021-02-19 04:54:55 -0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './modules/todo';
+import Todo from './models/todo.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'todo-database',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
